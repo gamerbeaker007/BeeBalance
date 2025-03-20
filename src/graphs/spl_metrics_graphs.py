73cff7e3dc9eedb27d3911b5bee597069bf9fb19
gamerbeaker <gamerbeaker@gmail.com>
2025-03-20 14:47:30 +0100

    Update layout of SPL metrics

---
+++ src/graphs/spl_metrics_graphs.py
@@ -0,0 +1,252 @@
+import streamlit as st
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+
+
+METRIC_NAMES = {
+    'battles': 'Battles',
+    'battles-modern': 'Battles Modern',
+    'battles-survival': 'Battles Survival',
+    'battles-wild': 'Battles Wild',
+    'dau': 'Daily Active Users',
+    'dec_rewards': 'DEC Rewards',
+    'market_vol': 'Market Volume',
+    'market_vol_usd': 'Market Volume USD',
+    'mkt_cap_usd': 'Market CAP USD',
+    'sign_ups': 'Signups',
+    'spellbooks': 'Spellbooks',
+    'tx_total': 'Total transactions',
+}
+
+
+def format_metric_name(metric):
+    """Converts metric keys to human-readable titles."""
+    return METRIC_NAMES.get(metric, metric.replace("-", " ").title())
+
+
+def add_user_join_line(fig, username, join_date, y_max):
+    fig.add_trace(
+        go.Scatter(
+            x=[join_date, join_date],
+            y=[0, y_max],
+            mode="lines",
+            line=dict(color="red", width=4),
+            name=f"Join date of {username}",
+            yaxis='y'  # Assign y-axis
+        )
+    )
+    return fig
+
+
+def create_market_graph(df, username, join_date, show_join_date):
+    # Filter DataFrame to include only the desired metrics
+    filtered_df = df[df["metric"].isin(["market_vol", "market_vol_usd", "mkt_cap_usd"])]
+
+    # Create a subplot figure with multiple y-axes
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
+
+    # Define colors for each metric
+    metric_colors = {
+        "market_vol": "blue",
+        "market_vol_usd": "purple",
+        "mkt_cap_usd": "orange"
+    }
+
+    max_mkt_cap = filtered_df[filtered_df["metric"] == "mkt_cap_usd"]["value"].max()*1.10
+    max_mkt_vol_usd = filtered_df[filtered_df["metric"] == "market_vol_usd"]["value"].max()*1.10
+    max_mkt_vol = filtered_df[filtered_df["metric"] == "market_vol"]["value"].max()*1.10
+
+    if join_date and show_join_date:
+        fig = add_user_join_line(fig, username, join_date, max_mkt_cap)
+
+    # Add traces for each metric
+    for metric in filtered_df.metric.unique():
+        metric_df = filtered_df[filtered_df["metric"] == metric]
+        metric_name = METRIC_NAMES.get(metric, metric)
+
+        # Determine which y-axis to use
+        if metric == "market_vol":
+            yaxis = "y3"  # Third y-axis
+        elif metric == "market_vol_usd":
+            yaxis = "y2"  # Secondary y-axis
+        else:
+            yaxis = "y"   # Primary y-axis
+
+        fig.add_trace(
+            go.Scatter(
+                x=metric_df["date"],
+                y=metric_df["value"],
+                mode="lines+markers",
+                marker=dict(size=5, color=metric_colors.get(metric, "red")),
+                name=metric_name,
+                yaxis=yaxis  # Assign y-axis
+            )
+        )
+
+
+    # Update layout with three y-axes
+    fig.update_layout(
+        xaxis=dict(tickformat="%Y-%m-%d"),
+        yaxis=dict(
+            title=METRIC_NAMES["mkt_cap_usd"],  # Primary Y-Axis
+            titlefont=dict(color=metric_colors["mkt_cap_usd"]),
+            tickfont=dict(color=metric_colors["mkt_cap_usd"]),
+            range=[0, max_mkt_cap],
+        ),
+        yaxis2=dict(
+            title=METRIC_NAMES["market_vol_usd"],  # Second Y-Axis (right)
+            titlefont=dict(color=metric_colors["market_vol_usd"]),
+            tickfont=dict(color=metric_colors["market_vol_usd"]),
+            overlaying="y",
+            side="right",
+            showgrid=False,
+            range=[0, max_mkt_vol_usd],
+        ),
+        yaxis3=dict(
+            title=METRIC_NAMES["market_vol"],  # Third Y-Axis (far right)
+            titlefont=dict(color=metric_colors["market_vol"]),
+            tickfont=dict(color=metric_colors["market_vol"]),
+            overlaying="y",
+            side="right",
+            position=1,  # Adjust position of third axis
+            showgrid=False,
+            range=[0, max_mkt_vol],
+        ),
+        xaxis_title="Date",
+        height=800,
+    )
+
+    # Display the plot
+    st.plotly_chart(fig)
+
+    with st.expander("Data", expanded=False):
+        st.dataframe(filtered_df)
+
+
+def create_user_graph(df, username, join_date, show_join_date):
+    # Filter DataFrame to include only the desired metrics
+    filtered_df = df[df["metric"].isin(["dau", "sign_ups", "spellbooks"])]
+
+    # Create a subplot figure with multiple y-axes
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
+
+    # Define colors for each metric
+    metric_colors = {
+        "dau": "blue",
+        "sign_ups": "purple",
+        "spellbooks": "orange"
+    }
+
+    max_dau = filtered_df[filtered_df["metric"] == "dau"]["value"].max()*1.10
+    max_sing_ups = filtered_df[filtered_df["metric"] == "sign_ups"]["value"].max()*1.10
+    max_spellbooks = filtered_df[filtered_df["metric"] == "spellbooks"]["value"].max()*1.10
+
+    if join_date and show_join_date:
+        fig = add_user_join_line(fig, username, join_date, max_spellbooks)
+
+    # Add traces for each metric
+    for metric in filtered_df.metric.unique():
+        metric_df = filtered_df[filtered_df["metric"] == metric]
+        metric_name = METRIC_NAMES.get(metric, metric)
+
+        # Determine which y-axis to use
+        if metric == "dau":
+            yaxis = "y3"  # Third y-axis
+        elif metric == "sign_ups":
+            yaxis = "y2"  # Secondary y-axis
+        else:
+            yaxis = "y"   # Primary y-axis
+
+        fig.add_trace(
+            go.Scatter(
+                x=metric_df["date"],
+                y=metric_df["value"],
+                mode="lines+markers",
+                marker=dict(size=5, color=metric_colors.get(metric, "red")),
+                name=metric_name,
+                yaxis=yaxis  # Assign y-axis
+            )
+        )
+
+    # Update layout with three y-axes
+    fig.update_layout(
+        xaxis=dict(tickformat="%Y-%m-%d"),
+        yaxis=dict(
+            title=METRIC_NAMES["spellbooks"],  # Primary Y-Axis
+            titlefont=dict(color=metric_colors["spellbooks"]),
+            tickfont=dict(color=metric_colors["spellbooks"]),
+            range=[0, max_spellbooks],
+        ),
+        yaxis2=dict(
+            title=METRIC_NAMES["sign_ups"],  # Second Y-Axis (right)
+            titlefont=dict(color=metric_colors["sign_ups"]),
+            tickfont=dict(color=metric_colors["sign_ups"]),
+            overlaying="y",
+            side="right",
+            showgrid=False,
+            range=[0, max_sing_ups],
+        ),
+        yaxis3=dict(
+            title=METRIC_NAMES["dau"],  # Third Y-Axis (far right)
+            titlefont=dict(color=metric_colors["dau"]),
+            tickfont=dict(color=metric_colors["dau"]),
+            overlaying="y",
+            side="right",
+            position=1,  # Adjust position of third axis
+            showgrid=False,
+            range=[0, max_dau],
+        ),
+        xaxis_title="Date",
+        height=800,
+    )
+
+    # Display the plot
+    st.plotly_chart(fig)
+
+    with st.expander("Data", expanded=False):
+        st.dataframe(filtered_df)
+
+def create_battle_graph(df):
+    filtered_df = df[df["metric"].str.startswith("battles")]
+
+
+    fig = go.Figure()
+
+    for metric in filtered_df.metric.unique().tolist():
+        if metric == 'battles-modern':
+            color = 'blue'
+        elif metric == 'battles-wild':
+            color = 'purple'
+        elif metric == 'battles-survival':
+            color = 'orange'
+        else:
+            color = 'red'
+
+        metric_df = filtered_df.loc[filtered_df.metric == metric]
+        fig.add_trace(
+            go.Scatter(
+                x=metric_df["date"],
+                y=metric_df["value"],
+                mode="lines+markers",
+                marker=dict(
+                    size=5,
+                    color=color,
+                    symbol="circle",
+                ),
+                name=METRIC_NAMES.get(metric),
+            )
+        )
+
+    # Format x-axis
+    fig.update_layout(
+        xaxis=dict(tickformat="%Y-%m-%d"),
+        yaxis_title="Amount of battles played",
+        xaxis_title="Date",
+        height=800,
+    )
+
+
+    # Display the plot
+    st.plotly_chart(fig)
+    with st.expander("Data", expanded=False):
+        st.dataframe(filtered_df)
--- src/pages/spl_metrics_page.py
+++ src/pages/spl_metrics_page.py
@@ -1,100 +1,99 @@
 import logging
+from datetime import datetime, timedelta
+
 import pandas as pd
+import pytz
 import streamlit as st
-import plotly.express as px
 
 from src.api import spl
+
+from src.graphs import spl_metrics_graphs
 
 log = logging.getLogger("SPL Metrics")
 
 
-METRIC_NAMES = {
-    'battles': 'Battles',
-    'battles-modern': 'Battles Modern',
-    'battles-survival': 'Battles Survival',
-    'battles-wild': 'Battles Wild',
-    'dau': 'Daily Active Users',
-    'dec_rewards': 'DEC Rewards',
-    'market_vol': 'Market Volume',
-    'market_vol_usd': 'Market Volume USD',
-    'mkt_cap_usd': 'Market CAP USD (Cards)',
-    'sign_ups': 'Signups',
-    'spellbooks': 'Spellbooks',
-    'tx_total': 'Total transactions',
-}
+
+def display_period_buttons():
+    # Define filter options
+    options = {
+        "All": None,
+        "12 Months": 365,
+        "6 Months": 180,
+        "1 Month": 30,
+        "2 Weeks": 14,
+        "7 Days": 7
+    }
+
+    # Create the toggle buttons
+    # Initialize session state for toggle buttons
+    if "selected_option" not in st.session_state:
+        st.session_state.selected_option = 7
+
+    # Layout for toggle buttons
+    cols = st.columns(len(options))
+
+    for i, (label, days) in enumerate(options.items()):
+        if cols[i].button(label, use_container_width=True):
+            st.session_state.selected_option = days
+
+    # Get the selected duration
+    return st.session_state.selected_option
 
 
-def format_metric_name(metric):
-    """Converts metric keys to human-readable titles."""
-    return METRIC_NAMES.get(metric, metric.replace("-", " ").title())
+def filter_on_days(df, number_of_days):
+    df["date"] = pd.to_datetime(df["date"])
+    now_utc = datetime.now(pytz.utc)
+    # Convert to datetime format
+    if number_of_days is not None:
+        filtered_df = df[df["date"] >= now_utc - timedelta(days=number_of_days)]
+    else:
+        filtered_df = df
+    return filtered_df
 
-
-def add_chart(df, title, slider_key):
-    df["date"] = pd.to_datetime(df["date"])  # Convert to datetime format
-
-    # Set default min/max date
-    min_date = df["date"].min().to_pydatetime()
-    max_date = df["date"].max().to_pydatetime()
-
-    # Store each slider's state separately
-    if slider_key not in st.session_state:
-        st.session_state[slider_key] = (min_date, max_date)
-
-    selected_range = st.slider(
-        f"Select Date Range for '{title}'",
-        min_value=min_date,
-        max_value=max_date,
-        value=st.session_state[slider_key],  # Use stored value
-        format="YYYY-MM-DD",
-        key=slider_key,  # Unique key to prevent re-rendering all sliders
-    )
-
-    # Convert selected range to timestamps
-    start_date = pd.Timestamp(selected_range[0])
-    end_date = pd.Timestamp(selected_range[1])
-
-    # Filter Data
-    filtered_df = df[(df["date"] >= start_date) & (df["date"] <= end_date)]
-
-    # Create Plotly Figure
-    fig = px.line(
-        filtered_df,
-        x="date",
-        y="value",
-        title=f"{title}",
-        labels={"date": "Date", "value": title},
-    )
-
-    # Add fill below the line with 50% transparency
-    fig.update_traces(line=dict(color="green", width=2), fill="tozeroy", fillcolor="rgba(0,128,0,0.5)")
-
-    # Format x-axis
-    fig.update_layout(
-        xaxis=dict(tickformat="%Y-%m-%d"),
-        yaxis_title=title,
-        xaxis_title="Date",
-        height=800,
-    )
-
-    # Display the plot
-    st.plotly_chart(fig)
-    with st.expander("Data", expanded=False):
-        st.dataframe(filtered_df)
+def create_one_dataframe(df, number_of_days):
+    df_exploded = df.explode("values")
+    # Normalize the dictionary column into separate columns
+    df_transformed = pd.json_normalize(df_exploded.to_dict("records"))
+    # Rename columns for clarity
+    df_transformed = df_transformed.rename(columns={"values.date": "date", "values.value": "value"})
+    # Convert date column to datetime format
+    df_transformed["date"] = pd.to_datetime(df_transformed["date"])
+    # Select final columns
+    df_transformed = df_transformed[["date", "metric", "value"]]
+    # Filtered asked number of days
+    filtered_df = filter_on_days(df_transformed, number_of_days)
+    return filtered_df
 
 
 def get_page():
     st.title("SPL Metrics")
 
     df = spl.get_metrics()
-    unique_metrics = df["metric"].unique()  # Get unique metric names
 
-    metric_mapping = {metric: format_metric_name(metric) for metric in unique_metrics}
-    selected_metric_friendly = st.selectbox("Select a Metric to Display", list(metric_mapping.values()))
-    selected_metric = next(k for k, v in metric_mapping.items() if v == selected_metric_friendly)
-    metric_df = pd.DataFrame(df[df.metric == selected_metric]["values"].iloc[0])
+    number_of_days = display_period_buttons()
+    df = create_one_dataframe(df, number_of_days)
 
-    # Unique slider key per metric
-    slider_key = f"slider_{selected_metric}"
+    username = st.text_input("Enter SPL user name (determine join date)")
+    show_join_date = st.checkbox("Show join date")
 
-    # Render the selected metric's chart
-    add_chart(metric_df, selected_metric_friendly, slider_key)
+    join_date = None
+    if username:
+        user_df = spl.get_player_details(username)
+        if user_df.empty:
+            st.warning("Incorrect username")
+        else:
+            join_date = pd.to_datetime(user_df["join_date"]).iloc[0]
+
+    tab1, tab2, tab3 = st.tabs(["Battle Metrics", "Card Market Metrics", "User Metrics"])
+    with tab1:
+        st.subheader("Battle Metrics")
+        spl_metrics_graphs.create_battle_graph(df)
+
+    with tab2:
+        st.subheader("Card Market Metrics")
+        spl_metrics_graphs.create_market_graph(df, username, join_date, show_join_date)
+
+    with tab3:
+        st.subheader("User Metrics")
+        spl_metrics_graphs.create_user_graph(df, username, join_date, show_join_date)
+

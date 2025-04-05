73ef0d376b7f357ffb5240455a3eda09579c0c4e
gamerbeaker <gamerbeaker@gmail.com>
2025-04-05 19:21:16 +0200

    Update query comment

--- src/pages/custom_queries_subpages/query_remark.py
+++ src/pages/custom_queries_subpages/query_remark.py
@@ -10,6 +10,6 @@ def add_section(num_accounts, params):
             - Hive Power (HP) - Between {params["hp_min"]} and {params["hp_max"]}
             - Reputation Score - Between {params["reputation_min"]} and {params["reputation_max"]}
             - Posting Rewards - Between  {params["posting_rewards_min"]} and {params["posting_rewards_max"]}
-            - Have made more than {params["posts"]} in the last {params["months"]}
+            - Have made more than {params["posts"]} posts in the last {params["months"]}
             - Have been active in the last {params["months"]} months with minimal {params["comments"]} comments
         """)
